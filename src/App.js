c5806296bee3546a701cad80a4b1e4ff65fe1d81
VARART <111012201+VARART@users.noreply.github.com>
2023-05-26 21:58:47 +0300

    On branch internship-fe-3-1 	modified:   .DS_Store 	new file:   src/.DS_Store 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/index.js 	new file:   src/sagas/firebaseSaga/chatSaga.js 	new file:   src/sagas/firebaseSaga/firebaseAuthSaga.js 	new file:   src/sagas/localStorageManager/saga.js 	new file:   src/sagas/quizSaga/quizManagerUserSaga.js 	new file:   src/sagas/rootSaga.js 	new file:   src/store/.DS_Store 	new file:   src/store/reducers/chatReducer.js 	new file:   src/store/reducers/index.js 	new file:   src/store/reducers/quizReducer.js 	new file:   src/store/reducers/userReducer.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      Hello people! I'm quiz-chat-fe-3 project
+    </div>
+  );
+}
+
+export default App;
